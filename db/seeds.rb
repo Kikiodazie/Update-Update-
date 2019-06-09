5eb3620e6ffda889f447279f64973719b5b09810
Kikiodazie <38439680+Kikiodazie@users.noreply.github.com>
2019-06-09 03:58:29 +0100

    Added Application Layout & CSS, Articles views, created Articles controllers to handle requests, added updated the migration file and rake manually inputed articles on seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Article.create(title: "Difficulty of Times", author: "Divine Odazie", body: "“It was a lot easier to build a tech startup in the 80s-90s because it was harder but it’s harder to build a tech startup now because it’s easier” -Divine Odazie.
+
+    I was discussing with a software developer about startups and this quote came into existence. 99% of startups all over the world, fail. Then if it comes to Nigeria (my home country), I really do not want to even mention it.
+    
+    Technology is very interesting, but it requires skill. Building tech startups was a lot easier before (when you have the skill) because it wasn’t something everyone could do. Tech was hard. But now, the reverse is the case — you do not need to be a tech guru to be involved in tech so it’s easy. That makes it hard for people who want to take it seriously. Everybody is doing it so why would anyone fund your startup?
+    
+    I will explain this by showing the differences between founding a company then (the 80s — 90s) and now. I’ll also show the difference between trying to innovate now and trying to innovate then.
+    
+    FOUNDING A STARTUP
+    
+    “Startup” was not a common word in the business scene as it is today. It was originally recorded in 1977 by Business Week: An incubator for startup companies — especially in the fast-growth, high-technology fields.
+    
+    The starting of tech companies (Tech Startups) in those days, was largely limited to geeks who were exploring this new tech called computers. These geeks had a strong technical background.
+    
+    Bill Gates had experience at Lakeside Private School and the University of Washington while Jobs’s tenure at Atari proves that they were technologically oriented. Larry Page and Sergey Brin are also great examples.
+    
+    For average individuals with ideas and little or no technical experience, building a tech startup was hard and practically impossible; but with the background of people like Steve Jobs, Larry Page, Gates, and Sergey Brin, it would be easier to effectively bring ideas to reality.
+    
+    Now, almost everyone wants to build or have a tech startup. It’s rising in popularity and burgeoning entrepreneurs are seeing it as a way to cash out big or Get a Home Run (Baseball).
+    
+    With quick and easy access to a vast amount of data through the internet, budding entrepreneurs can learn virtually any tech skill. Platforms like Udemy, Codecademy, Udacity offer great learning opportunities. Entrepreneurs can also get teams of highly experienced techies from platforms like Xteam and Toptal and bring their ideas to reality.
+    
+    So, the fact that someone may be building the same product you are building or even a better one for the same market or another, brings about COMPETITION which makes entrepreneurs lose sight of INNOVATION as Peter Thiel said in his book, Zero to One.
+    
+    A typical example is the story of Peter Thiel’s Confinity and Elon Musk’s X.com that brought about PayPal. Most people don’t know about that. I also had my personal experiences while building Solvehelp: my first startup. I realized after almost a year that another platform with the same idea had been rolled out and it had more than 10K active users. It was painful but lessons were learned, and experiences were gotten.
+    
+    INNOVATION IN STARTUPS
+    
+    We will all agree that in the last 30 years, there have been tremendous technological advancements — like AI (Artificial intelligence), VR (Virtual Reality) and AR (Augmented Reality). The rise of tech giants like Amazon, Google, and Facebook also occurred within that period.
+    
+    During that period, there were few dominating tech giants so there was a great room for innovation in technology. Then came the dot.com boom (you must have heard about that). Money was pumped into tech though most of the dot.com companies failed (and lessons were learned). Those that survived became billion — dollar companies. They focused not much on money but on innovation.
+    
+    As I said, there was much room for innovation in technology and many great and geeky minds were eager to try it out. I believe that’s the major reason why it was easier for them. More ‘un-taken’ opportunities.
+    
+    — just as you wouldn’t go trying to improve chemistry by looking for new elements or a better periodic table. It may be possible but it’s definitely far-fetched. In the same vein, technology always looks like there’s nothing more to innovate but we all know that it’s just an illusion. There are still innovations to make.
+    
+    Okay, so from here on, I’m going to speak in quotes — from myself and others.
+    
+    “Innovation by definition is risky” –Mauro Porcini; and it means “Going from Zero to One”- Peter Thiel. From nothing to something worth using. Something that can stand the test of time with improvement because “The passage of time changes the technological preference of users”-Divine Odazie. That’s why tech companies always build new features and buy new and innovative startups to keep the innovation going. This is very hard to do — to get an innovative idea that will sell at this time where virtually everyone wants to build startups. I will advise you to “Start with why?”- Simon Sinek: the reason for your idea. If you can get potential users to buy into the reason for your product, it would sell.
+    
+    I would say it’s a hard feat to achieve and I’m writing from my own experience.")
+Article.create(title: "Is there someone who can watch us?", author: "Matt Healy", body: "I used to have a reoccurring dream when I was younger. The narrative was loosely based, but the setting was always the surrounding areas of where my parents grew up. I think I was slightly obsessed with how certain, bleak areas of Northern England contained my history and the soul of my family, my psyche - I always felt connected and disconnected at the same time. Metaphorically and physically I would visit these places frequently as a youngster and in turn have used them as the setting to a lot of the stories in The 1975 - the North East was the visual setting to my formative years.")
+Article.create(title: "Between the stripes", author: "Leonardo San Miguel", body: 'Pantomime lore dates as far back as the eight century BC, but it was never formalized as an artthe one we know now we know nowuntil around the sixteenth century in Easter Europe. Although almost every record of Pantomime implies the art was primarily conceived as a form of entertainment, there have always been traces that indicate the art was a manifestation of a grander concept: that of revelation through absence. Indeed, a pantomimes performance is, however comical or banal, a form of illusionism. As author Edward Lancaster writes in The Mysticism of Performing Arts, There is no greater demonstration of magic than that of creating entire worlds and realities through the use of nothing but ones body in motion and the unrelenting desire of the human mind to fill in the blanks.
+')
+Article.create(title: "Water", author: "Diana Storm", body: "Benjamin is laying on a field of grass and it is the middle of a windy, but warm day. He is staring at the sky. Blue looking at blue, but hidden under a pair of aviators. His hair cannot yet decide if it is a dark yellow or a light brown, and he likes that.")
