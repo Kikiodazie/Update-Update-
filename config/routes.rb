44b5d208791e1a511d3b0c13f9bdfc9dafc79be2
Kikiodazie <38439680+Kikiodazie@users.noreply.github.com>
2019-06-15 13:33:12 +0100

    Added Login,signup and Logout functionalities with sessions(using bcrypt gem and has_secure_password to hash users password)

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :articles
   root "articles#index"
+  resources :users
+  get 'signup' => 'users#new'
+  get '/login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_09_020142) do
+ActiveRecord::Schema.define(version: 2019_06_10_232140) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "author"
     t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
